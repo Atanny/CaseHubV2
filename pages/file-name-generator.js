c9a1a73d2d04e670d59685742c6484712fe3448e
Atanny <155293074+Atanny@users.noreply.github.com>
2026-08-19 13:40:49 +0800

    Delete pages directory

--- pages/file-name-generator.js
+++
@@ -1,7 +0,0 @@
-import Layout from '../components/layout/Layout';
-import RequireAuth from '../components/layout/RequireAuth';
-import FileNameGeneratorPage from '../components/pages/FileNameGenerator';
-
-export default function FileNameGeneratorRoute() {
-  return <RequireAuth><Layout><FileNameGeneratorPage/></Layout></RequireAuth>;
-}
