08313658bbcd7ff968399b41f86dcee41bfbedb8
Atanny <155293074+Atanny@users.noreply.github.com>
2026-08-26 13:49:03 +0800

    Delete src/utils directory

--- src/utils/cls.js
+++
@@ -1,7 +0,0 @@
-/**
- * Joins conditional class names, filtering out falsy values.
- * Usage: cls('base', isActive && 'active', isDisabled && 'disabled')
- */
-export function cls(...args) {
-  return args.filter(Boolean).join(' ');
-}
